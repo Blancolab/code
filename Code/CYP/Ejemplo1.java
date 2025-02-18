a865bd8f20e86258e7b0865c4770db4b09154c3d
Blancolab <mozartmarlone@icloud.com>
2025-02-18 15:19:48 -0600

    Add files via upload

---
+++ Code/CYP/Ejemplo1.java
@@ -0,0 +1,28 @@
+
+
+public class Ejemplo1{
+
+	public static void main(String args[]){
+		// enteros
+		byte ladosDado;
+		short variable1, variable2;
+		int variable3 = 69;
+		long variable4;
+
+		//decimal
+		float precio = 2.56f;
+		double litrosGasolina = 2.56;
+		
+		//character
+		char letra = 'a';
+
+		//booleano
+		boolean bandera = false;
+
+		System.out.println("El precio es: " + precio + "\n\"hola\"");
+
+		
+
+	}
+
+}
